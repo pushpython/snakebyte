c5561ad87b0e98624910f69816472a9fd1188a23
pushpython <39602945+pushpython@users.noreply.github.com>
2018-05-24 22:02:24 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,14 +10,7 @@ assemblyManager=AssemblyManager() # main assembley manager object
 
 cpu.memory.SetCellVal(0x001,"0x013") # setting cell 19 to 1
 
-assembly=[
-    "INP",
-    "STA 19",
-    "INP",
-    "ADD 19",
-    "OUT",
-    "HLT"
-          ] # assembly code for program
+assembly=EnviromentalInterface.Interface()
 
 instructions=assemblyManager.ParseAssembly(assembly) # parsing assembly to hex with memory location
 cpu.memory.LoadInstructions(instructions) # loading instructions into memory
