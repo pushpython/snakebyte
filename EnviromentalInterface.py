2da3d67457a7139ecda46e6b4852fcde95eec1e9
pushpython <39602945+pushpython@users.noreply.github.com>
2018-05-24 22:00:36 +0100

    Update EnviromentalInterface.py

--- EnviromentalInterface.py
+++ EnviromentalInterface.py
@@ -1,15 +1,13 @@
 
+class EnviromentalInterface:
 
+    # programming enviroment
+    def EnviromentInterface(self):
+        instructions=[]
+        while True:
+            console_input=input(">")
+            if console_input == "c": EnviromentalInterface()
+            elif console_input == "END": break
+            else: instructions.append(console_input)
+        return instructions
 
-# programming enviroment
-def EnviromentInterface():
-    instructions=[]
-    while True:
-        console_input=input(">")
-        if console_input == "c": EnviromentalInterface()
-        elif console_input == "END": break
-        else: instructions.append(console_input)
-    return instructions
-
-instructions=EnviromentInterface()
-print(instructions)
