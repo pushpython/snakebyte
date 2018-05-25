3aae6736b5c5e25ba155fef06ff195c51fbb7288
pushpython <39602945+pushpython@users.noreply.github.com>
2018-05-25 12:04:58 +0100

    Update Memory.py

--- Memory.py
+++ Memory.py
@@ -2,24 +2,31 @@ from Cell import Cell
 
 
 class Memory():
+    
+    # Initialisation
     def __init__(self):
-        self.cells = self.GenerateCells(64) # Initialises the memory
+        # Initialises the memory cells
+        self.cells = self.GenerateCells(64)
 
-    def GenerateCells(self,size): # Initialises all memory cells
-
+    # Initialises all memory cells with size 'size'
+    def GenerateCells(self,size):
+        
+        # Creating array of cells, each cell provided an index i
         cells = [Cell(hex(i)) for i in range(size-1)]
-
-        # cells=[]
-        # for i in range(size-1):
-        #     cells.append(Cell(hex(i)))
+        
+        # Return the cells array
         return cells
 
-    def GetCellVal(self,loc): # getting the value of a cell
+    # Getting the value of a cell
+    def GetCellVal(self,loc):
+        # Converting the hex index to an integer
         index = int(loc, 0)
+        # Returning the value of the cell with given index
         return self.cells[index].GetValue()
 
-
-    def SetCellVal(self, val, loc): # setting the value of a cell with location loc
+    # Function to set cell value with given value and location
+    def SetCellVal(self, val, loc):
+        # Converting the hex index to an integer
         index = int(loc,0)
         self.cells[index].SetValue(val)
         return True
