0101c9622b05e687d5b1361cb2db4cf12222ff70
pushpython <39602945+pushpython@users.noreply.github.com>
2018-05-25 09:43:03 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,7 +11,7 @@ assemblyManager=AssemblyManager() # main assembley manager object
 while True:
     assembly=EnviromentalInterface.Interface()
     instructions=assemblyManager.ParseAssembly(assembly) # parsing assembly to hex with memory location
-    if instructions == None: print("...")
+    if instructions == None: print("Restarting")
     else:
         cpu.memory.LoadInstructions(instructions) # loading instructions into memory
         cpu.Execute()
