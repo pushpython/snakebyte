ef7e7720ce611ff6a6bf487013dcdf846bd19b73
pushpython <39602945+pushpython@users.noreply.github.com>
2018-05-25 11:54:58 +0100

    Update Cell.py

--- Cell.py
+++ Cell.py
@@ -1,10 +1,17 @@
+# Class defining a single memory location
 class Cell():
+    
+    # Initialisation
     def __init__(self,loc):
+        # Memory location index
         self.location=loc
+        # Default value (in hex)
         self.value="0x000"
-
+    
+    # Function to set value
     def SetValue(self,val):
         self.value=val
-
+        
+    # Function to get value
     def GetValue(self):
         return self.value
