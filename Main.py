a1f56f5d42862669e19c9bc54f8f4071b11bd091
pushpython <39602945+pushpython@users.noreply.github.com>
2018-05-25 09:44:36 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,11 +8,11 @@ from EnviromentalInterface import EnviromentalInterface
 cpu=CPU() # main CPU object
 assemblyManager=AssemblyManager() # main assembley manager object
 
-while True:
-    assembly=EnviromentalInterface.Interface()
-    instructions=assemblyManager.ParseAssembly(assembly) # parsing assembly to hex with memory location
-    if instructions == None: print("Restarting")
-    else:
-        cpu.memory.LoadInstructions(instructions) # loading instructions into memory
-        cpu.Execute()
+while True: # Main loops, isn't nessescary for running program
+    assembly=EnviromentalInterface.Interface() # Getting array of assembly instructions from the console
+    instructions=assemblyManager.ParseAssembly(assembly) # Parsing assembly to hex with memory location
+    if instructions == None: print("Restarting") # If there is an error in the instructions, loop again
+    else: # If no errors...
+        cpu.memory.LoadInstructions(instructions) # Loading instructions into memory
+        cpu.Execute() # Executing program
 
